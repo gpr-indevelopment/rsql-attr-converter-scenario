a3e8ad65220c0eda7d2ed7af7535f9a35650c576
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-12-15 14:46:59 -0300

    Spring initializr

---
+++ src/main/java/com/test/rsqlattrconverterscenario/RsqlAttrConverterScenarioApplication.java
@@ -0,0 +1,13 @@
+package com.test.rsqlattrconverterscenario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsqlAttrConverterScenarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsqlAttrConverterScenarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/rsqlattrconverterscenario/RsqlAttrConverterScenarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.rsqlattrconverterscenario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsqlAttrConverterScenarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
